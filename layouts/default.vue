c3048da169d571a43e289ff1d3b5a5fc3cf8c257
神颖 <396938503@qq.com>
2019-09-02 15:01:17 +0800

    第一次提交

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!-- 渲染头部组件 -->
+   <Header/>
+    <!-- 页面的占位符.类似于roter-view -->
+    <nuxt />  
+    footer
+  </div>
+</template>
+<script>
+//导入header头部组件
+import Header from '@/components/header';
+export default {
+  //注册组件
+  components:{
+    Header
+  }
+}
+</script>
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*{
+  margin:0;
+  padding:0;
+}
+ul,li,ol{
+  list-style:none;
+}
+a{
+  text-decoration: none;
+  color:inherit;
+}
+/* a:hover{} */
+em,i{
+  font-style:normal;
+}
+</style>
---
+++ pages/air/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>机票模块</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
