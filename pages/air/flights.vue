1316aea242f5d5d293c934ae5d39510819282836
神颖 <396938503@qq.com>
2019-09-10 17:22:10 +0800

    航班列表以及分页部分

---
+++ pages/air/flights.vue
@@ -0,0 +1,128 @@
+<template>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <FlightsListHead/>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                <FlightsItem
+                v-for="(item, index) in dataList"
+                :key="index"
+                :data="item"/>
+                  
+                  <!-- 分页 -->
+                  <!-- size-change每页条数切换时候触发 -->
+                  <!-- current-change页码切换时候触发 -->
+                  <!-- current-page当前的页码 -->
+                  <!-- page-size当前显示的条数 -->
+                  <!-- total总条数 -->
+                <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="pageIndex"
+                    :page-sizes="[5, 10, 15, 20]"
+                    :page-size="pageSize"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="total">
+                 </el-pagination>
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
+</template>
+
+<script>
+
+import FlightsListHead from "@/components/air/flightsListHead.vue"
+import FlightsItem from "@/components/air/flightsItem.vue"
+
+
+export default {
+    data(){
+        return {
+            //机票列表返回的总数据.总数据包含4个属性,flights/info /options/tatol
+            flightsData:{},
+
+            dataList:[],
+
+            pageIndex:1,//当前的页码
+            pageSize:5,//当前的条数
+            total:0  //总条数
+
+        }
+    },
+    components:{
+        FlightsListHead,
+        FlightsItem
+    },
+    mounted(){
+        //这个可以查看路由返回的所有的信息
+        // console.log(this.$route)
+
+        this.$axios({
+            url:"airs",
+            params:this.$route.query
+        }).then(res=>{
+            console.log(res.data)
+            //赋值给总数据
+            this.flightsData=res.data;
+
+            //分页的总条数
+            this.total=this.flightsData.flights.length;
+
+            //第一页的值
+        this.dataList=this.flightsData.flights.slice(0,this.pageSize);
+        })
+    },
+    methods:{
+        //每页条数切换时候触发
+        handleSizeChange(val){
+            this.pageSize=val
+
+                         //按照数学公式切换dataList的值
+            this.dataList=this.flightsData.flights.slice(0,val);
+        },
+        //页码切换的时候触发
+        handleCurrentChange(val){
+            this.pageIndex = val;//当前页
+
+            //按照数学公式切换dataList的值
+            this.dataList=this.flightsData.flights.slice(
+                (this.pageIndex-1) * this.pageSize,
+                 this.pageIndex * this.pageSize);
+
+        },
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
+
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
+</style>
